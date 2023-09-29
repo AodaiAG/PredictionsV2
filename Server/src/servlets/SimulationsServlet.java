e5321876cc067d4be62566e21330413ad5dd61b7
Sec4life.$ao <de7ket.3yonk.a@hotmail.com>
2023-09-30 02:09:26 +0300

    uploadServlet Done

--- Server/src/servlets/SimulationsServlet.java
+++ Server/src/servlets/SimulationsServlet.java
@@ -1,15 +1,20 @@
 package servlets;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import pDTOS.WorldDTO;
 import pSystem.Engine;
+import pSystem.World;
+import pSystem.aSimulation;
 import utils.ServletUtils;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 import java.util.Set;
 @WebServlet(name = "SimulationsList",urlPatterns = "/simulations")
 public class SimulationsServlet extends HttpServlet
@@ -17,15 +22,29 @@ public class SimulationsServlet extends HttpServlet
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
     {
-        //returning JSON objects, not HTML
+
         response.setContentType("application/json");
-        try (PrintWriter out = response.getWriter())
-        {
-            Gson gson = new Gson();
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
             Engine engine = ServletUtils.getEngine(getServletContext());
-            String json = gson.toJson(engine.getAllSimulations().keySet());
-            out.println(json);
-            out.flush();
+            Map<String, aSimulation> simulationMap=engine.getAllSimulations();
+        for ( Map.Entry<String, aSimulation> entry : simulationMap.entrySet())
+        {
+            String key = entry.getKey();
+            aSimulation aSimulation = entry.getValue();
+            WorldDTO worldDTO=engine.convertWorldToDTO(aSimulation.getWorld());
+            String jsonResponse = gson.toJson(worldDTO);
+            System.out.println(jsonResponse);
+            try (PrintWriter out = response.getWriter())
+            {
+                out.println(jsonResponse);
+            }
         }
+
+
+            //
+
+
+
     }
 }
