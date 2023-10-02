e15489d3c2a602f2bc04ff60ad3a9cb804e837e5
OrGamlliel <orgm@mta.ac.il>
2023-10-02 18:30:29 +0300

    allocation begin

---
+++ Server/src/servlets/AllocationsServlet.java
@@ -0,0 +1,21 @@
+package servlets;
+
+import Requests.RequestManager;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import utils.ServletUtils;
+
+import java.io.IOException;
+
+@WebServlet(name = "AllocationServlet", urlPatterns = "/allocations")
+public class AllocationsServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestManager requestManager = ServletUtils.getRequestManager(this.getServletContext());
+
+    }
+}
