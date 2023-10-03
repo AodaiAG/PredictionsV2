a04f0b77ac81c41e2a93e1eb3d3d4f28c364b690
OrGamlliel <orgm@mta.ac.il>
2023-10-03 12:15:05 +0300

    allocations

--- Server/src/servlets/AllocationsServlet.java
+++ Server/src/servlets/AllocationsServlet.java
@@ -1,14 +1,20 @@
 package servlets;
 
 import Requests.RequestManager;
+import Requests.SimulationRequest;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import utils.ServletUtils;
+import utils.SessionUtils;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Set;
 
 @WebServlet(name = "AllocationServlet", urlPatterns = "/allocations")
 public class AllocationsServlet extends HttpServlet {
@@ -16,6 +22,38 @@ public class AllocationsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         RequestManager requestManager = ServletUtils.getRequestManager(this.getServletContext());
+        Object clientType = req.getAttribute("type");
+        if(clientType != null)
+        {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+            String clientTypeString = clientType.toString();
+            if(clientType.equals("admin"))
+            {
+                Set<SimulationRequest> simulationRequests = requestManager.getRequests();
+
+                String jsonResponse = gson.toJson(simulationRequests);
+                try (PrintWriter out = resp.getWriter())
+                {
+                    out.println(jsonResponse);
+                }
+
+            }
+            else if(clientType.equals("user"))
+            {
+                Set<SimulationRequest> userSimulationReq = requestManager.getUserRequests(SessionUtils.getUsername(req));
+                String jsonResponse = gson.toJson(userSimulationReq);
+                try (PrintWriter out = resp.getWriter())
+                {
+                    out.println(jsonResponse);
+                }
+            }
+        }
+        else
+        {
+            resp.getWriter().println("invalid query parameter");
+            return;
+        }
 
     }
 }
